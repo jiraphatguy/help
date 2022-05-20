1e9f16fcec81754c425a6e4696b37dac301e0a72
Jiraphat Phulprapha <guyguy@GMacBook.local>
2022-05-20 17:39:15 +0700

    help

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="w-full bg-yellow-400 h-12"></div>
+     <nuxt-link to="/test" class="nav-link">Login</nuxt-link>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, deserunt. Quisquam animi molestiae illo quod, eaque reiciendis facere, nulla saepe dolores nisi excepturi magnam a esse odit voluptate amet dignissimos.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+@Component({})
+export default class Index extends Vue {
+
+}
+</script>
---
+++ pages/test/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>test</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+@Component({})
+export default class Test extends Vue {}
+</script>
